1f9554a42726e1316619aae9177011d45a409036
RPiGPIO <brownluckkapankha@gmail.com>
2026-04-06 00:12:21 +0530

    Update r10.py

--- r10.py
+++ r10.py
@@ -27,26 +27,42 @@
 # Install if needed:
 # sudo apt install python3-rpi.gpio
 
+# PRACTICAL 10 - SMOKE SENSOR WITH LED ALERT
+# Smoke Sensor DO -> GPIO17 -> Pin 11
+# LED -> GPIO27 -> Pin 13
+# GND -> Pin 6
+# 5V -> Pin 2
+
 import RPi.GPIO as GPIO
 import time
 
-SMOKE = 17
-LED = 27
+SMOKE_DO = 17
+LED_PIN = 27
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SMOKE, GPIO.IN)
-GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(SMOKE_DO, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+print("Smoke sensor warming up... wait 20 seconds")
+GPIO.output(LED_PIN, GPIO.LOW)
+time.sleep(20)
+
+print("System Ready")
+print("Monitoring smoke/gas...")
 
 try:
     while True:
-        if GPIO.input(SMOKE) == 0:
-            print("🚨 Smoke Detected")
-            GPIO.output(LED, GPIO.HIGH)
-        else:
-            print("Air Clean")
-            GPIO.output(LED, GPIO.LOW)
+        smoke_detected = (GPIO.input(SMOKE_DO) == 0)
 
-        time.sleep(0.5)
+        if smoke_detected:
+            print("🚨 SMOKE/GAS DETECTED")
+            GPIO.output(LED_PIN, GPIO.HIGH)
+        else:
+            print("Air is clean")
+            GPIO.output(LED_PIN, GPIO.LOW)
+
+        time.sleep(1)
 
 except KeyboardInterrupt:
+    print("Exiting...")
     GPIO.cleanup()
